fcf341ad7845431d35bfe7439dee6c4ed0f587b2
FeniaBatzoni <fbatzoni@threenitas.com>
2023-09-24 23:13:21 +0300

    UserVM

---
+++ ProductCard/wwwroot/js/main.js
@@ -0,0 +1,12 @@
+﻿
+
+export function checkMobileScreen() {
+    // Get the window's inner width
+    var windowWidth = window.innerWidth;
+
+    // Define a threshold width below which it's considered a mobile screen
+    var mobileThreshold = 768; // You can adjust this threshold as needed
+
+    // Return true if it's a mobile screen, false otherwise
+    return windowWidth < mobileThreshold;
+}
